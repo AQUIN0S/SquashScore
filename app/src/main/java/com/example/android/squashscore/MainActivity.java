6bb366864f42e4ea3ef53b3e5358778c6f50d4f9
AQUIN0S <aquinos@acyonix.com>
2017-06-10 23:08:02 +1200

    Initial commit

---
+++ app/src/main/java/com/example/android/squashscore/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.android.squashscore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int pointsPlayer1 = 0;
+    private int gamesPlayer1 = 0;
+    private int pointsPlayer2 = 0;
+    private int gamesPlayer2 = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /*
+    * Finish this method!
+    public void addPointPlayer1(View view); {
+
+        TextView t = (TextView) findViewById(R.id.score_player_1);
+
+
+    }
+    */
+
+}
